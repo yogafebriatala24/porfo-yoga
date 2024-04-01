fa4463a3499b8c6b696a7251513d3202ebb94cc8
Yoga Febriatala <yogafebriatala24@gmail.com>
2024-04-02 00:08:50 +0700

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import Head from "next/head";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
@@ -71,17 +72,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-BG5T0LPHN3"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {` window.dataLayer = window.dataLayer || [];
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-BG5T0LPHN3"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-BG5T0LPHN3');
          `}
-      </Script>
+        </Script>
+      </Head>
       <body className={`${poppins.className} bg-bg-image`}>{children}</body>
     </html>
   );
